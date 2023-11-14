7bbf0f88bd6d260930ecbd315337f4f88a0a9602
Cat Ngo <ngohoangngocanh2000@gmail.com>
2023-11-14 11:12:31 -0500

    Commit everything

---
+++ Assets/Scripts/Devscripts/RotateSky.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RotateSky : MonoBehaviour
+{
+    public float RotateSpeed = 1.3f;
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotateSpeed);
+
+
+
+    }
+}
